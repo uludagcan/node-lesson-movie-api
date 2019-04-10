efdf75ec2bb0a9fcd1250439931ed6bec51dffde
uludagcan <uludagcan91@gmail.com>
2019-04-10 23:46:23 +0300

    /api/directors post method

--- app.js
+++ app.js
@@ -6,10 +6,11 @@ const logger = require('morgan');
 
 const indexRouter = require('./routes/index');
 const movieRouter = require('./routes/movie');
+const directorRouter = require('./routes/director');
 
 const app = express();
 //db connection
-//const db = require('./helper/db.js')();
+const db = require('./helper/db.js')();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -22,6 +23,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/api/movies', movieRouter);
+app.use('/api/directors', directorRouter);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
---
+++ routes/director.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+
+const Director = require('../models/Director');
+router.post('/', (req, res, next) => {
+    const director = new Director(req.body);
+    const promise = director.save();
+
+    promise.then((data) => {
+        res.json(data);
+    }).catch((err) => {
+        res.json(err);
+    });
+});
+
+module.exports = router;
