fd8fe905dd591ee9f57295f91bb098a367b1dab3
uludagcan <uludagcan91@gmail.com>
2019-04-11 15:22:27 +0300

    jsonwebtoken verify

--- app.js
+++ app.js
@@ -14,6 +14,10 @@ const db = require('./helper/db.js')();
 //Config
 const config = require('./config');
 app.set('api_secret_key', config.api_secret_key);
+
+//Middleware
+const verifyToken = require('./middleware/verify-token');
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -25,6 +29,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+app.use('/api/', verifyToken);
 app.use('/api/movies', movieRouter);
 app.use('/api/directors', directorRouter);
 
