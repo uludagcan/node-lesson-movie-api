a13704b8e8ca671b6e131ce94378c98d0c7ffbdc
uludagcan <uludagcan91@gmail.com>
2019-04-11 11:37:31 +0300

    validations

--- models/Movie.js
+++ models/Movie.js
@@ -5,12 +5,30 @@ const MovieSchema = new Schema({
     director_id: Schema.Types.ObjectId,
     title: {
         type: String,
-        required: true
+        required: [true, '`{PATH}` alanı zorunludur'],
+        maxlength: 40,
+        minlength: 1
     },
-    category: String,
-    country: String,
-    year: Number,
-    imdb_score: Number,
+    category: {
+        type: String,
+        maxlength: 30,
+        minlength: 1
+    },
+    country: {
+        type: String,
+        maxlength: 30,
+        minlength: 1
+    },
+    year: {
+        type: Number,
+        max: 2040,
+        min: 1900
+    },
+    imdb_score: {
+        type: Number,
+        max: 10,
+        min: 0
+    },
     createdAt: {
         type: Date,
         default: Date.now
