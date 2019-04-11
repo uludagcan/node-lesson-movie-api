923e0c4c968ceb0175f668b2b4093e00491600b5
uludagcan <uludagcan91@gmail.com>
2019-04-11 10:16:48 +0300

    /api/directors/:director_id get method

--- routes/director.js
+++ routes/director.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
 
 const Director = require('../models/Director');
+
 router.post('/', (req, res, next) => {
     const director = new Director(req.body);
     const promise = director.save();
@@ -15,6 +17,56 @@ router.post('/', (req, res, next) => {
 
 router.get('/', (req, res) => {
     const promise = Director.aggregate([
+        {
+            $lookup: {
+                from: 'movies',
+                localField: "_id",
+                foreignField: "director_id",
+                as: 'movie'
+            }
+        },
+        {
+            $unwind: {
+                path: '$movie',
+                preserveNullAndEmptyArrays: true
+            }
+        },
+        {
+            $group: {
+                _id: {
+                    _id: '$_id',
+                    name: '$name',
+                    surname: '$surname',
+                    bio: '$bio'
+                },
+                movies: {
+                    $push: '$movie'
+                }
+            }
+        },
+        {
+            $project: {
+                _id: '$_id._id',
+                name: '$_id.name',
+                surname: '$_id.surname',
+                movies: '$movies'
+            }
+        }
+    ]);
+    promise.then((data) => {
+        res.json(data);
+    }).catch((err) => {
+        res.json(err);
+    })
+});
+
+router.get('/:director_id', (req, res) => {
+    const promise = Director.aggregate([
+        {
+            $match: {
+                '_id': mongoose.Types.ObjectId(req.params.director_id)
+            }
+        },
         {
             $lookup: {
                 from: 'movies',
