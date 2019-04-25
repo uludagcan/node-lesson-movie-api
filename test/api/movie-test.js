a7e2bf489e2616c321c2adf53c357cd6fc02df72
uludagcan <uludagcan91@gmail.com>
2019-04-25 18:29:27 +0300

    /api/movies PUT/director_id test

--- test/api/movie-test.js
+++ test/api/movie-test.js
@@ -78,6 +78,35 @@ describe('/api/movies tests', () => {
                 });
         });
     });
+
+    describe('/PUT:director_id movie', () => {
+        it('it should UPDATE a movie given by id', (done) => {
+            const movie = {
+                title: '93Creative',
+                director_id: '5caf13414323c50eb002c2da',
+                category: 'Suç',
+                country: 'Fransa',
+                year: 1950,
+                imdb_score: 8
+            };
+            chai.request(server)
+                .put('/api/movies/' + movieId)
+                .send(movie)
+                .set('x-access-token', token)
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.be.a('object');
+                    res.body.should.have.property('title').eql(movie.title);
+                    res.body.should.have.property('director_id').eql(movie.director_id);
+                    res.body.should.have.property('category').eql(movie.category);
+                    res.body.should.have.property('country').eql(movie.country);
+                    res.body.should.have.property('year').eql(movie.year);
+                    res.body.should.have.property('imdb_score').eql(movie.imdb_score);
+
+                    done();
+                });
+        });
+    });
 });
 
 
