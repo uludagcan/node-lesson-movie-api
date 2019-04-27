fc4da4de4177f761732efb963100e96476e93dcd
uludagcan <uludagcan91@gmail.com>
2019-04-27 11:33:08 +0300

    travisci

--- test/api/index.test.js
+++ test/api/index.test.js
@@ -1,12 +1,13 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
+const should = chai.should();
 const server = require('../../app');
 
 chai.use(chaiHttp);
 
 describe('Node Server', () => {
     it('(GET /) returns the homepage.', (done) => {
-        chai.request(`http://localhost:3000`)
+        chai.request(server)
             .get('/')
             .end((err, res) => {
 
